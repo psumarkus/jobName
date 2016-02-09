7ca2da09972e954b817972a1a593d1b29929d101
doktorbrown <tevanbrown@gmail.com>
2016-02-09 18:34:50 -0500

    Update jobName.py
    added direct printing to label machine

--- jobName.py
+++ jobName.py
@@ -1,27 +1,30 @@
 '''
-Created on Feb 4, 2016
+Created on Feb 9, 2016
 
 @author: tbrown
 '''
+import os
 import re
-from bs4 import BeautifulSoup #installation at http://www.crummy.com/software/BeautifulSoup/
+from bs4 import BeautifulSoup #installation at http://www.crummy.com/software/BeautifulSoup/     $ pip install beautifulsoup4
+
+
 
 fname = "MakerBot Innovation Center.html"
-soup = BeautifulSoup(open(fname)) 
+soup = BeautifulSoup(open(fname), 'html.parser') 
 out = "clipboard.txt"
 
 #print soup.prettify()
 
 nameGetter = soup.find_all(text = re.compile("^Requestor"))[0].next
 emailGetter = soup.find_all(text = re.compile("^Requestor Email"))[0].next.next.next_element
-#filamentGetter = soup.find_all(text = re.compile("Filament Usage Actual"))[0].next
+filamentGetter = soup.find_all(text = re.compile("Filament Usage Actual"))[0].next
 #notesGetter = soup.find_all(text = re.compile("^Campus"))#not yet implemented in form
 
 print nameGetter
 print emailGetter
-#print filamentGetter
+print filamentGetter
 #print notesGetter
-print " "#space the final frontier
+#print " "#space the final frontier
 
 #open output file for writing results
 f = open(out, 'w')
@@ -33,3 +36,5 @@ f.writelines(lineOne)
 print lineOne
 
 f.close()
+
+os.system("lpr -P DYMO_LabelWriter_450_Turbo clipboard.txt")
