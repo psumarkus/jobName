0c138c94c467faa3a81663de6582b21a627a40bc
doktorbrown <tevanbrown@gmail.com>
2016-02-14 19:56:39 -0500

    Update jobName.py
    added date and verified lpf formatting

--- jobName.py
+++ jobName.py
@@ -1,10 +1,12 @@
 '''
-Created on Feb 9, 2016
+Created on Feb 14, 2016
 
 @author: tbrown
 '''
 import os
 import re
+import time
+from datetime import date
 from bs4 import BeautifulSoup #installation at http://www.crummy.com/software/BeautifulSoup/     $ pip install beautifulsoup4
 
 
@@ -17,10 +19,12 @@ out = "clipboard.txt"
 
 nameGetter = soup.find_all(text = re.compile("^Requestor"))[0].next
 emailGetter = soup.find_all(text = re.compile("^Requestor Email"))[0].next.next.next_element
+#campusGetter = soup.find_all(text = re.compile())
 #printerGetter = soup.find_all(text = re.compile("^Printer"))[0].next
 #filamentGetter = soup.find_all(text = re.compile("Filament Usage Actual"))[0].next
 #notesGetter = soup.find_all(text = re.compile("^Campus"))#not yet implemented in form
-
+today =str(date.today())
+print today
 print nameGetter
 print emailGetter
 #print printerGetter
@@ -35,7 +39,7 @@ f = open(out, 'w')
 #format extracted text for label
 #lineOne = (nameGetter,'\n', emailGetter, '\n', "\n Failing to add a Raft or \n Supports when preparing the \n .makerbot file is the most \n common reason for a failed print. \n Please check: \n makercommons.psu.edu/fail \n for more info.  Consultations \n can be scheduled by emailing  \n makercommons@psu.edu.")
 #for landscape print
-lineOne = (nameGetter,'\n', emailGetter, '\n', '\n', "Not adding a Raft or Supports when   prepping the .makerbot file is the   most common reason for failed prints. \n Info: makercommons.psu.edu/fail \n Consultation Scheduling: \n makercommons@psu.edu")
+lineOne = (nameGetter, today, " ", emailGetter, '\n', '\n', "Not adding a Raft or Supports when   prepping the .makerbot file is the   most common reason for failed prints. \n Info: makercommons.psu.edu/fail \n Consultation Scheduling: \n makercommons@psu.edu")
 
 
 f.writelines(lineOne)
